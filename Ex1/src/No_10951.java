17049fe0a39f9843ca2bba03869610fa7bb16636
minwoo <minwoo@DESKTOP-USJT9MB>
2020-09-27 23:12:27 +0900

    No.10951

---
+++ Ex1/src/No_10951.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class No_10951 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int A,B;
+		System.out.print("A입력: ");
+		A = in.nextInt();
+		System.out.print("B입력: ");
+		B = in.nextInt();
+		
+		int sum = A + B;
+		
+		System.out.print("A + B = "+sum);
+
+	}
+
+}
