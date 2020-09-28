5ea2f7037d7cd1d7cdc6bf2c0e7de91857d46318
minwoo <hyminwoo0000@gmail.com>
2020-09-28 17:01:13 +0900

    No.10951

--- Ex_1/src/No_10951.java
+++ Ex_1/src/No_10951.java
@@ -5,10 +5,10 @@ public class No_10951 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int A,B;
-		System.out.print("정수 A입력: ");
+		System.out.print("Input A: ");
 		A = in.nextInt();
 		
-		System.out.print("정수 B입력: ");
+		System.out.print("Input B: ");
 		B = in.nextInt();
 		int sum = A+B;
 		System.out.println("A+B= "+sum);
